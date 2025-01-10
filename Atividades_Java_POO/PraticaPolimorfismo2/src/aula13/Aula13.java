cb157ce538bc8f53f62f769e9066e400aaccd053
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-01-09 22:15:08 -0300

    Atividade pratica de  polimorfismo 2

---
+++ Atividades_Java_POO/PraticaPolimorfismo2/src/aula13/Aula13.java
@@ -0,0 +1,12 @@
+package aula13;
+
+public class Aula13 {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+
+        c.reagir("Olá!");
+        c.reagir(false);
+        c.reagir(19,45);
+        c.reagir(19,5.0f);
+    }
+}
