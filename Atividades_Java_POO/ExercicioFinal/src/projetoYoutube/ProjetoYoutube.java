ffdaef6fc8485e9cbca5a55ce776e4e2ca9dd5c7
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-01-10 23:34:51 -0300

    final atividade poo

--- Atividades_Java_POO/ExercicioFinal/src/projetoYoutube/ProjetoYoutube.java
+++ Atividades_Java_POO/ExercicioFinal/src/projetoYoutube/ProjetoYoutube.java
@@ -13,12 +13,24 @@ public class ProjetoYoutube {
         g[0] = new Gafanhoto("ygor",24,"M","ygs");
         g[1] = new Gafanhoto("Bruno", 23, "M", "bru");
 
+
+        Visualizacao[] vis = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0],v[2]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+
+        vis[1] = new Visualizacao(g[0],v[1]);
+        vis[1].avaliar(87.0f);
+        System.out.println(vis[0].toString());
+
+        /*
         System.out.println("VIDEOS\n----------------------------");
         System.out.println(v[0].toString());
         System.out.println(v[1].toString());
         System.out.println(v[2].toString());
         System.out.println("\nGAFANHOTOS\n---------------------------");
         System.out.println(g[0].toString());
-        System.out.println(g[1].toString());
+        System.out.println(g[1].toString());*/
     }
 }
