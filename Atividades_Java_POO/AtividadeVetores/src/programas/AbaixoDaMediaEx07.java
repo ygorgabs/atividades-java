0e6d599e5a4ec612f3708ca17fe8038cfd6457b6
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-01-25 19:13:56 -0300

    Exercicio vetores pt5

--- Atividades_Java_POO/AtividadeVetores/src/programas/AbaixoDaMediaEx07.java
+++ Atividades_Java_POO/AtividadeVetores/src/programas/AbaixoDaMediaEx07.java
@@ -1,10 +1,33 @@
 package programas;
 
+import java.util.Scanner;
+
 public class AbaixoDaMediaEx07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ent = new Scanner(System.in);
+		System.out.print("Quantos elementos vai ter o vetor? ");
+		int num = ent.nextInt();
+		double soma = 0, media = 0;
+		double[] vetor = new double[num];
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print("Digite um número: ");
+			vetor[i] = ent.nextDouble();
+			
+			soma += vetor[i];
+		}
+		
+		media = soma/vetor.length;
+		System.out.printf("%nMedia do vetor = %.3f %n",media);
+		
+		System.out.println("Elementos abaixo da média: ");
+		for (double d : vetor) {
+			if (d < media)
+				System.out.println(d);
+		}
+		
+		ent.close();
 	}
 
 }
