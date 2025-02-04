986ac3165f64490cae69457fdd3bd2308a660109
Ygor Gabriel Silva <silvaygor16@gmail.com>
2025-02-03 22:20:10 -0300

    String builder pt2

--- 04-Recursos_Avancados/Exercicios/AtividadeStringBuilder/src/application/Program.java
+++ 04-Recursos_Avancados/Exercicios/AtividadeStringBuilder/src/application/Program.java
@@ -1,7 +1,23 @@
 package application;
 
+import entities.Comment;
+import entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comment c1 = new Comment("Tenha uma boa viagem!!");
+        Comment c2 = new Comment("Uau! Que incrível!");
+        Post p1 = new Post(sdf.parse("21/06/2024 13:05:44"), "Viagem para Nova Zelandia,", "Vou visitar esse país maravilhoso", 12);
+
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        System.out.println(p1);
 
     }
 }
